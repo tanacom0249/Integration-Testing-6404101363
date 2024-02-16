a4adf03311ad726ac73dd05fd7cf806d2e5ff7ad
tanacom0249 <119926673+tanacom0249@users.noreply.github.com>
2024-02-16 10:38:44 +0700

    book-id

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/books', (req, res) => {
   res.json(books)
 })
 
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
+  
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
