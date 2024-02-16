f94979fbd36336a0f2cddd17de6a92a3933dc99c
tanacom0249 <119926673+tanacom0249@users.noreply.github.com>
2024-02-16 10:28:53 +0700

    books

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+const books = require('./db')
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
